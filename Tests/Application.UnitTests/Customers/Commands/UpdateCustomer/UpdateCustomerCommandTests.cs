1c5d4f83c8d44c90f0d991ea3d094cbb1de4971c
Braden Dressendorfer <48044240+Brad3nD@users.noreply.github.com>
2020-04-02 10:15:24 -0500

    Added UpdateCustomerTest

---
+++ Tests/Application.UnitTests/Customers/Commands/UpdateCustomer/UpdateCustomerCommandTests.cs
@@ -0,0 +1,23 @@
+﻿using Northwind.Application.Customers.Commands.UpdateCustomer;
+using System.Threading;
+using Northwind.Application.UnitTests.Common;
+using Xunit;
+
+namespace Northwind.Application.UnitTests.Customers.Commands.CreateCustomer
+{
+    public class UpdateCustomerCommandTests : CommandTestBase
+    {
+        [Fact]
+        public void Handle_GivenValidRequest_ShouldRaiseCustomerUpdatedNotification()
+        {
+            var sut = new UpdateCustomerCommand.Handler(_context);
+            var validId = "QAZQ1";
+
+            var result = sut.Handle(new UpdateCustomerCommand { Id = validId }, CancellationToken.None);
+            var customer = _context.Customers.FindAsync(validId);
+            var newId = "L10S1";
+
+            Assert.Equal(validId, newId);
+        }
+    }
+}
