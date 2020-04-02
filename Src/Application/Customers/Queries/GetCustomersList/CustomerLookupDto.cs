66330b1a517e0f475600fb42f4ea1e1acdfde6df
Braden Dressendorfer <48044240+Brad3nD@users.noreply.github.com>
2020-04-02 10:12:00 -0500

    Updated CustomerLookupDto.cs

--- Src/Application/Customers/Queries/GetCustomersList/CustomerLookupDto.cs
+++ Src/Application/Customers/Queries/GetCustomersList/CustomerLookupDto.cs
@@ -8,12 +8,21 @@ namespace Northwind.Application.Customers.Queries.GetCustomersList
     {
         public string Id { get; set; }
         public string Name { get; set; }
+        public string Address { get; set; }
+        public string Region { get; set; }
+        public string Phone { get; set; }
+        public string PostalCode { get; set; }
 
         public void Mapping(Profile profile)
         {
             profile.CreateMap<Customer, CustomerLookupDto>()
                 .ForMember(d => d.Id, opt => opt.MapFrom(s => s.CustomerId))
-                .ForMember(d => d.Name, opt => opt.MapFrom(s => s.CompanyName));
+                .ForMember(d => d.Name, opt => opt.MapFrom(s => s.CompanyName))
+                .ForMember(d => d.Address, opt => opt.MapFrom(s => s.Address))
+                .ForMember(d => d.Region, opt => opt.MapFrom(s => s.Region))
+                .ForMember(d => d.Phone, opt => opt.MapFrom(s => s.Phone))
+                .ForMember(d => d.PostalCode, opt => opt.MapFrom(s => s.PostalCode));
+
         }
     }
 }
